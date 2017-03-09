0fcc806743d79a32f6befa2d7ee7a930df7f97dd
Pouramua <poukupenga@gmail.com>
2017-03-10 11:03:16 +1300

    Refactoring seed file

--- seeds/cohort.js
+++ seeds/cohort.js
@@ -8,6 +8,6 @@ exports.seed = function(knex, Promise) {
         {id: 1, name: 'Aidan', food: 'Pasta', hobbies: 'Playing video games'},
         {id: 2, name: 'Pou', food: 'Butter Chicken', hobbies: 'Fishing'},
         {id: 3, name: 'Kat', food: 'Edamame', hobbies: 'Hiking'}
-      ]);
-    });
-};
+      ])
+    })
+}
